bb7f38698aebf2f15a62f30bb20559a6b866019f
wensen <wensen@88.com>
2023-06-08 17:31:45 +0800

    项目开发完成

---
+++ src/pages/home/homemain.vue
@@ -0,0 +1,21 @@
+<template>
+    <headerCom></headerCom>
+    <div class="container" id="container">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import headerCom from '../../components/headerCom.vue';
+
+</script>
+
+<style lang='scss' scoped>
+.container {
+    width: 100vw;
+    min-height: 100vh;
+    padding: 4px 16px;
+    background-color: #f4f5f9;
+}
+</style>
